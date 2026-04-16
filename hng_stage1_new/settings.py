e9e0ef9584425b581f84aed8830fb610783587b1
DouglasEmmanuel <emmanueldouglas2121@gmail.com>
2026-04-16 15:49:10 -0700

    Configure database settings with environment variable and update DATABASES configuration for Supabase

--- hng_stage1_new/settings.py
+++ hng_stage1_new/settings.py
@@ -13,10 +13,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+load_dotenv()
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print(type(os.environ.get("DATABASE_URL")))
+print(os.environ.get("DATABASE_URL"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -80,17 +82,22 @@ WSGI_APPLICATION = 'hng_stage1_new.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-# 	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=True # Supabase requires SSL for remote connections
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
